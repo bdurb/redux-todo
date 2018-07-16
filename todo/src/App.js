b5ae9200eb8c5115a38fefd5bcb2ba7f8483eb60
bdurb <brian@rpmgreenville.com>
2018-07-16 12:41:44 -0700

    set up provider on index.js, working on basic file structure and component layout

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Gon put some todos n such
+        <h1>These are the Todos!</h1>
+        < Todos />
       </div>
     );
   }
---
+++ todo/src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todos = () => {
+  return (
+    <div>
+      Some Todos will go here
+    </div>
+  );
+}
+ 
+export default Todos;
