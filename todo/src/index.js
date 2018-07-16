87e07bc572336699e3a2e5fe99799a81c039e12b
bdurb <brian@rpmgreenville.com>
2018-07-16 12:47:23 -0700

    setting up reducer, removed registerserviceworker

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,14 +4,11 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import todosReducer from './reducers/todosReducer';
 
-const store = createStore(() => {
-  todos: []
-})
+const store = createStore(todosReducer);
 
 ReactDOM.render(<Provider store={store}>
   <App />
   </Provider>,
   document.getElementById('root'));
-registerServiceWorker();
---
+++ todo/src/reducers/todosReducer.js
@@ -0,0 +1,6 @@
+
+const todosReducer = () => {
+  return {
+    
+  }
+}
