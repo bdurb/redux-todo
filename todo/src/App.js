839ba3238c93a1b4c2c06dffb30ef19d952e157b
bdurb <brian@rpmgreenville.com>
2018-07-16 11:56:17 -0700

    created react app todo

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Gon put some todos n such
+      </div>
+    );
+  }
+}
+
+export default App;
